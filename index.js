002edfdac2b66c635834a0221d5a1327968d748c
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-08-22 20:11:25 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+  let messages = [];
+  names.forEach((name) => {
+    messages.push("Thank you, " + name + ", for the wonderful surprise gift!");
+  });
+  return messages;
+}
+
+console.log(writeCards(names));
+function countDown () {
+let count = 10;
+while (count > -1) {
+  console.log(count--);
+}
+}
